b6e3d4e7f87d1174c82a39ada00f7b12525cad7c
Gembali Sandesh Kumar <93411433+gembalisandesh@users.noreply.github.com>
2024-09-19 00:14:50 +0530

    Initial Commit

---
+++ ColourPickerApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ColourPickerApp
+//
+//  Created by Equipp on 19/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
